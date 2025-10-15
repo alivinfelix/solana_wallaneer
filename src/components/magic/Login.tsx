ff40a41487f910c04ac8f7c08a2d9dc077cf0a9c
proskillowner <proskillowner@gmail.com>
2025-10-15 14:42:04 +0000

    Refactor Login component layout for improved responsiveness; hide Header on mobile devices and adjust global styles for the login page, including card dimensions and padding for better user experience.

--- src/components/magic/Login.tsx
+++ src/components/magic/Login.tsx
@@ -7,9 +7,10 @@ import SocialLogin from './auth/SocialLogin';
 const Login = ({ token, setToken }: LoginProps) => {
   return (
     <div className="login-page min-h-screen">
-      {/* <BackgroundDecorations /> */}
-      <Header />
-      <div className={`max-w-[100%] grid grid-cols-1 md:grid-cols-2 grid-flow-row auto-rows-fr gap-4 md:gap-5 p-2 md:p-4 mt-4 md:mt-8`}>
+      <div className="hidden md:block">
+        <Header />
+      </div>
+      <div className={`max-w-[100%] grid grid-cols-1 md:grid-cols-2 grid-flow-row auto-rows-fr gap-4 md:gap-5 p-4 md:p-4 mt-4 md:mt-8`}>
         <EmailOTP token={token} setToken={setToken} />
         <SocialLogin token={token} setToken={setToken} />
       </div>
